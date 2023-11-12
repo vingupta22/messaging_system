5c85ef41d81b29e17eb2717dfdffd355a3c35a07
FancyGohst <94806384+FancyGohst@users.noreply.github.com>
2023-11-12 13:11:02 -0500

    Update Message.java
    Added test cases for each of the methods

--- Message.java
+++ Message.java
@@ -5,14 +5,12 @@ public class Message {
     private String senderID;
     private String recipientID;
     private String timeStamp;
-    private boolean isDisappearing;
 
     public Message(String content, String senderID, String recipientID, String timeStamp) {
         this.content = content;
         this.senderID = senderID;
         this.recipientID = recipientID;
         this.timeStamp = timeStamp;
-        this.isDisappearing = false;
     }
 
     public void editMessage(String newMessage) {
@@ -35,11 +33,38 @@ public class Message {
         return timeStamp;
     }
 
-    public boolean isDisappearing() {
-        return this.isDisappearing;
-    }
-
-    public void setDisappearing(boolean disappearing) {
-        this.isDisappearing = disappearing;
+    public static void main(String[] args) {
+        Message first = new Message("Hello World", "Customer1", "Seller1", "13:09:13.103653700");
+        System.out.println("Test 1 for getContent method");
+        if (first.getContent().equals("Hello World")) {
+            System.out.println("getContent method passed the test case");
+        } else {
+            System.out.println("There's an error in the getContent method");
+        }
+        System.out.println("Test 2 for getSenderID method");
+        if (first.getSenderID().equals("Customer1")) {
+            System.out.println("getSenderID method passed the test case");
+        } else {
+            System.out.println("There's an error in the getSenderID method");
+        }
+        System.out.println("Test 3 for getRecipientID method");
+        if (first.getRecipientID().equals("Seller1")) {
+            System.out.println("getRecipientID method passed the test case");
+        } else {
+            System.out.println("There's an error in the getRecipientID method");
+        }
+        System.out.println("Test 4 for getTimeStamp method");
+        if (first.getTimeStamp().equals("13:09:13.103653700")) {
+            System.out.println("getTimeStamp method passed the test case");
+        } else {
+            System.out.println("There's an error in the getTimeStamp method");
+        }
+        System.out.println("Test 5 for editMessage method");
+        first.editMessage("Hello Earth");
+        if (first.getContent().equals("Hello Earth")) {
+            System.out.println("getContent method passed the test case");
+        } else {
+            System.out.println("There's an error in the getContent method");
+        }
     }
 }
